8b975a6d83d77de273c8babc22f9fa6a2780a316
femersky <forte201253@gmail.com>
2020-04-01 23:14:21 +0300

    posting hint

--- angular-client/src/app/services/hints.service.ts
+++ angular-client/src/app/services/hints.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Hint } from '../models/hint';
+import { UsersService } from './users.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,27 @@ import { Hint } from '../models/hint';
 export class HintsService {
   private baseUrl = 'http://localhost:3000/api/hints';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private users: UsersService) {}
 
   getByAuthor(author): Observable<Hint[]> {
     const url = `${this.baseUrl}/author/${author}`;
     return this.http.get<Hint[]>(url);
+  }
+
+  postHint(
+    title: string,
+    tags: string[],
+    author: string,
+    user_id: string
+  ): Observable<Object> {
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer ' + this.users.getUserFromLocalStorage().token
+    });
+
+    return this.http.post(
+      this.baseUrl,
+      { title, tags, author, user_id },
+      { headers }
+    );
   }
 }
